edd61a41534e0abfd720845cd1e2598adfe48d39
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-11-02 12:16:56 +0300

    Completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,24 @@
 class Song
     attr_accessor :name, :artist, :genre
 
-    @@count=0
-    @@genres=[]
-    @@artists=[]
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@genre_count = {}
+    @@artist_count = {}
 
     def initialize(name,artist,genre)
         @name = name
         @artist = artist
         @genre = genre
-        @@count +=1
-        @@genres << genre
+        @@count += 1
         @@artists << artist
+        @@genres << genre
+        @@genre_count[genre] ||= 0
+        @@genre_count[genre] += 1
+        @@artist_count[artist] ||= 0
+        @@artist_count[artist] += 1
+
     end
 
     def name=(name)
@@ -31,7 +38,10 @@ class Song
     end
 
     def self.genre_count
-        
+        @@genre_count
+    end
+    def self.artist_count
+        @@artist_count
     end
 end
 
